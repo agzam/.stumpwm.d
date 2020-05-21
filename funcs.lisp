bc672f15a7d1c54782e33bbcec40e4606a800eec
Ag Ibragimov <agzam.ibragimov@gmail.com>
2020-05-20 23:50:37 -0700

    within-split-p function

--- funcs.lisp
+++ funcs.lisp
@@ -5,3 +5,10 @@
   "Runs the comand and pops to the top menu. Useful for binding in interactive keymaps."
   (stumpwm::run-prog *shell-program* :args (list "-c" cmd) :wait nil)
   (pop-top-map))
+
+(defun within-split-p (&rest windows)
+  "Returns T if windows are on the same display and all are visible, i.e. in a split."
+  (let* ((wins (flatten windows))
+         (frames (mapcar 'window-head wins)))
+    (and (apply 'eq frames)
+         (every 'window-visible-p wins))))
